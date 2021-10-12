fbb36e1f9031c0d50350021f07ccfb4361f020b0
Stephen Mann <steve@mann.ws>
2021-10-12 19:15:30 -0400

    Changed 'eyes' to 'peek'

--- lid/NoDelay-Function-Eyes/NoDelay-Function-Eyes.ino
+++ lid/NoDelay-Function-Eyes/NoDelay-Function-Eyes.ino
@@ -22,29 +22,29 @@
 #include <NoDelay.h>
 
 //---------- prototypes ----------
-void yellow_ON();
-void yellow_OFF();
+void peek_ON();
+void peek_OFF();
 void eyes_ON();
 void eyes_OFF();
 void eyes_DIM();
 
 
 //Create noDelay objects
-noDelay yellowLED_onTime(1000, yellow_ON , false);
-noDelay yellowLED_offTime(1000, yellow_OFF, false);
+noDelay peekLED_onTime(1000, peek_ON , false);
+noDelay peekLED_offTime(1000, peek_OFF, false);
 noDelay eyesLED_onTime(1000, eyes_ON , false);
 noDelay eyesLED_offTime(1000, eyes_OFF, false);
 noDelay eyesLED_dim(20, eyes_DIM , true);           //How fast to dim the LED. Lower is faster
 
 const int LED_ON = 1;
 const int LED_OFF = 0;
-const int LED_YELLOW_PIN = D2;
+const int LED_PEEK_PIN = D2;
 const int LED_EYE_PIN = D1;
 
 
 int eyeVal = 0;
 const int EYES_MIN = 5;            //Eye will dim to this value in loop
-const int EYES_MAX = 100;          //Yellow_ON will bring eye back to this level.
+const int EYES_MAX = 100;          //peek_ON will bring eye back to this level.
 
 
 // ---------- setup ----------
@@ -55,22 +55,22 @@ void setup() {
   Serial.println(F("NoDelay-Function-Experiments.ino"));
   Serial.println();
 
-  pinMode(LED_YELLOW_PIN, OUTPUT);
+  pinMode(LED_PEEK_PIN, OUTPUT);
   pinMode(LED_EYE_PIN, OUTPUT);
-  digitalWrite(LED_YELLOW_PIN, LED_OFF);
+  digitalWrite(LED_PEEK_PIN, LED_OFF);
   digitalWrite(LED_EYE_PIN, LED_OFF);
 
 
   //Start the LED timers
-  yellow_ON();                     //The ON functions will start OFF functions.
+  peek_ON();                     //The ON functions will start OFF functions.
   eyes_ON();
 }
 
 
 // ---------- loop ----------
 void loop() {
-  yellowLED_onTime.update();        //If time has past, run set function
-  yellowLED_offTime.update();
+  peekLED_onTime.update();        //If time has past, run set function
+  peekLED_offTime.update();
   eyesLED_onTime.update();
   eyesLED_offTime.update();        //Blink eye
   eyesLED_dim.update();            //Dim eye
@@ -78,21 +78,21 @@ void loop() {
 }
 
 
-void yellow_ON() {
-  //The yellow ON timer has dinged.
+void peek_ON() {
+  //The peek ON timer has dinged.
   //Turn on the LED, stop the ON timer then start the OFF timer.
-  digitalWrite(LED_YELLOW_PIN, LED_ON);                   //Turn on the yellow LED
-  yellowLED_onTime.stop();                                //Stop the ON timer
-  yellowLED_offTime.setdelay(random(1000, 5000));         //LED will be on for this time.
-  yellowLED_offTime.start();                              //Start the OFF timer
+  digitalWrite(LED_PEEK_PIN, LED_ON);                   //Turn on the peek LED
+  peekLED_onTime.stop();                                //Stop the ON timer
+  peekLED_offTime.setdelay(random(1000, 5000));         //LED will be on for this time.
+  peekLED_offTime.start();                              //Start the OFF timer
   eyeVal = EYES_MAX;
 }
 
-void yellow_OFF() {
-  digitalWrite(LED_YELLOW_PIN, LED_OFF);                 //Turn off the yellow LED
-  yellowLED_offTime.stop();                              //Stop the OFF timer
-  yellowLED_onTime.setdelay(random(1000, 6000));         //LED will be off for this time.
-  yellowLED_onTime.start();                              //Start the ON timer
+void peek_OFF() {
+  digitalWrite(LED_PEEK_PIN, LED_OFF);                 //Turn off the peek LED
+  peekLED_offTime.stop();                              //Stop the OFF timer
+  peekLED_onTime.setdelay(random(1000, 6000));         //LED will be off for this time.
+  peekLED_onTime.start();                              //Start the ON timer
 }
 
 void eyes_ON() {
