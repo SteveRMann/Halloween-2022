49080310d35027f482e4d2e8b1ed307b1c6c9b95
Stephen Mann <steve@mann.ws>
2021-10-14 01:04:18 -0400

    Update loop.ino
    Removed peek stuff

--- src/Lid/loop.ino
+++ src/Lid/loop.ino
@@ -3,20 +3,14 @@ void loop() {
   ArduinoOTA.handle();
   mqttReconnect();          //Make sure we stay connected to the mqtt broker
   button.tick();            //Has a button been pressed?
-  menu();
 
-/*
-  //Blink the eyes
+  //Check the timers
   eyesLED_onTime.update();
   eyesLED_offTime.update();        //Blink eye
+  eyesLED_dim.update();            //Dim eye
 
-  //Fade the eyes
-  eyesLED_dim.update();     //Dim the eyes
-
-  //Open the lid at random times.
-  peekOnTime.update();        //If time has past, run set function
-  peekOffTime.update();
-*/  
-  //syncCheck();                   //Check the sync status.
+  menu();
+  syncCheck();              //Check the sync status.
+  eyes_DIM();
 
 }
