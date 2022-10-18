f4c8e0e18f82db6490be1d882c9b92ee60dc0d27
Stephen Mann <steve@mann.ws>
2022-10-18 13:37:23 -0400

    Cloned lid.ino to lid2.ino
    Removed OTA, MQTT, etc.

---
+++ src/lid2/setup.ino
@@ -0,0 +1,80 @@
+//=============== setup ===============
+void setup() {
+  pinMode(closedSwitch, INPUT_PULLUP);
+  pinMode(openSwitch, INPUT_PULLUP);
+  pinMode (BUTTON_PIN, INPUT_PULLUP);
+  pinMode (LOOP_PIN, INPUT_PULLUP);
+  pinMode(BLUE_LED_PIN, OUTPUT);
+  analogWrite(MOTOR_PIN, 0);             //PWM pin, start at zero.
+  pinMode(EYES_PIN, OUTPUT);
+  pinMode(FAN_PIN, OUTPUT);
+
+  beginSerial();
+
+  //lidState = 0;     //Open
+
+  attachInterrupt(BUTTON_PIN, actionButtonHandler, RISING);
+  attachInterrupt(LOOP_PIN, loopButtonHandler, RISING);
+
+  //  button.attachDoubleClick(doubleclick);
+  //  button.attachClick(singleClick);
+  //  button.attachLongPressStop(longPress);
+
+
+  //Cycle through the eyes' LED intensity.
+  Serial.println(F("Test the eyes."));
+  for (int j = 0; j < 4; j++) {
+    for (int i = 10; i < 255; i++) {
+      analogWrite(EYES_PIN, i);
+      delay(2);
+    }
+    for (int i = 255; i > 10; i--) {
+      analogWrite(EYES_PIN, i);
+      delay(2);
+    }
+  }
+  eyesVal = EYES_MIN;
+  analogWrite(EYES_PIN, eyesVal);
+  delay(1000);
+
+  //Fan
+  Serial.println(F("Test the fan."));
+  analogWrite(FAN_PIN, FAN_MAX);
+  delay(2000);
+  analogWrite(FAN_PIN, 0);
+  delay(1000);                                //Wait a second before opening the lid
+
+  //Lid
+  Serial.println(F("Test the lid."));
+  loopFlag = false;
+  openTheLid();
+  delay(1000);
+  closeTheLid();
+
+  loopFlag = false;
+  dbgLoopFlg();
+
+  analogWrite(FAN_PIN, FAN_MIN);
+
+
+  //Start the timers
+  eyes_ON();
+  //eyes_OFF();
+
+/*
+  // ========== MOTORTEST ==========
+  // Neverending function to find the minimum torque needed
+  // to run the motor
+  maxTorque = 255;
+  while (1) {
+    sensorValue = analogRead(sensorPin);
+    runTorque = sensorValue;
+    Serial.print(F("runTorque= "));
+    Serial.println(runTorque);
+    startTheMotor();
+    delay(1000);
+  }
+*/
+
+Serial.println(F("===================="));
+} //End of setup()
